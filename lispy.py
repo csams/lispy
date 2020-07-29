739803a366393406e81ed3be8a3263a66c5a6c7e
Christopher Sams <csams@redhat.com>
2020-07-29 16:34:35 -0500

    Break apart combinators so they're more understandable.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- lispy.py
+++ lispy.py
@@ -80,21 +80,13 @@ def evaluate(expression, environment=None):
 
 
 # See https://en.wikipedia.org/wiki/Fixed-point_combinator
-
 # For recursion in strict languages.
-Z = (lam, "f",
-        ((lam, "x",
-            ("f", ((lam, "v",
-                (("x", "x"), "v"))))),
-            (lam, "x",
-                ("f", ((lam, "v",
-                    (("x", "x"), "v")))))))
+v = (lam, "v", (("x", "x"), "v"))
+x = (lam, "x", ("f", v))
+Z = (lam, "f", (x, x))
 
 # For recursion in lazy languages.
 # Won't work with the current impl. Makes neat python core dumps when you blow
 # the stack, though.
-Y = (lam, "h",
-        ((lam, "x",
-            ("h", ("x", "x"))),
-            (lam, "x",
-                ("h", ("x", "x")))))
+x = (lam, "x", ("f", ("x", "x")))
+Y = (lam, "f", (x, x))
